606c165a4d678da1e2313bd40ff95c8fa031f8cb
Hassan220022 <hassansonson2002@gmail.com>
2023-11-15 10:08:28 +0200

    modified:   all_specifiers.c modified:   helpers2.c modified:   main.h modified:   numeros.c modified:   params.c

--- all_specifiers.c
+++ all_specifiers.c
@@ -5,28 +5,27 @@
  * @s: formatted string of characters
  *
  * Return: Always 0
-*/
+ */
 
 int (*get_specify(char *s))(va_list args, params_t *params)
 {
-	specify_t all[] =
-			{
-					{"c", print_char},
-					{"d", print_int},
-					{"%", print_percent},
-					{"s", print_string},
-					{"i", print_int},
-					{"b", _print_binary},
-					{"o", print_octal},
-					{"u", print_unsigned},
-					{"x", print_hex},
-					{"X", print_HEX},
-					{"p", print_address},
-					{"S", print_String},
-					{"r", print_rev},
-					{"R", print_rot13},
-					{NULL, NULL}
-			};
+	specify_t all[] = {
+			{"c", print_char},
+			{"d", print_int},
+			{"%", print_percent},
+			{"s", print_string},
+			{"i", print_int},
+			{"b", print_binary},
+			{"o", print_octal},
+			{"u", print_unsigned},
+			{"x", print_hex},
+			{"X", print_HEX},
+			{"p", print_address},
+			{"S", print_String},
+			{"r", print_rev},
+			{"R", print_rot13},
+			{NULL, NULL},
+		};
 	int seen = 0;
 
 	while (all[seen].specify)
@@ -47,11 +46,11 @@ int (*get_specify(char *s))(va_list args, params_t *params)
  * @params: the parameters
  *
  * Return: the bytes number
-*/
+ */
 
 int get_print_func(char *s, va_list args, params_t *params)
 {
-	int (*f) (va_list, params_t *) = get_specify(s);
+	int (*f)(va_list, params_t *) = get_specify(s);
 
 	if (f)
 		return (f(args, params));
@@ -64,7 +63,7 @@ int get_print_func(char *s, va_list args, params_t *params)
  * @params: the parameters
  *
  * Return: void
-*/
+ */
 
 int get_flag(char *s, params_t *params)
 {
@@ -72,21 +71,21 @@ int get_flag(char *s, params_t *params)
 
 	switch (*s)
 	{
-		case '+':
-			fl = params->plus_flag = 1;
-			break;
-		case ' ':
-			fl = params->space_flag = 1;
-			break;
-		case '#':
-			fl = params->hashtag_flag = 1;
-			break;
-		case '-':
-			fl = params->minus_flag = 1;
-			break;
-		case '0':
-			fl = params->zero_flag = 1;
-			break;
+	case '+':
+		fl = params->plus_flag = 1;
+		break;
+	case ' ':
+		fl = params->space_flag = 1;
+		break;
+	case '#':
+		fl = params->hashtag_flag = 1;
+		break;
+	case '-':
+		fl = params->minus_flag = 1;
+		break;
+	case '0':
+		fl = params->zero_flag = 1;
+		break;
 	}
 	return (fl);
 }
@@ -97,7 +96,7 @@ int get_flag(char *s, params_t *params)
  * @params: the parameters
  *
  * Return: void
-*/
+ */
 
 int get_modifier(char *s, params_t *params)
 {
@@ -105,12 +104,12 @@ int get_modifier(char *s, params_t *params)
 
 	switch (*s)
 	{
-		case 'h':
-			mod = params->h_modifier = 1;
-			break;
-		case 'l':
-			mod = params->l_modifier = 1;
-			break;
+	case 'h':
+		mod = params->h_modifier = 1;
+		break;
+	case 'l':
+		mod = params->l_modifier = 1;
+		break;
 	}
 	return (mod);
 }
@@ -122,13 +121,14 @@ int get_modifier(char *s, params_t *params)
  * @params: the parameters
  *
  * Return: pointer
-*/
+ */
 
 char *get_width(char *s, params_t *params, va_list args)
 {
 	int wide = 0;
 
-	if (*s == '*') {
+	if (*s == '*')
+	{
 		wide = va_arg(args, int);
 		s++;
 	}
