9edcf032ff40638637f344ed2882fda5728331ea
Hassan220022 <hassansonson2002@gmail.com>
2023-11-15 10:23:07 +0200

    0&1func.c -> 0_1_func.c adv.c printf.c

--- printf.c
+++ printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (p = (char *) format; *p; p++)
+	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, args);
 		if (*p != '%')
@@ -39,7 +39,8 @@ int _printf(const char *format, ...)
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specify(p))
-			sum += print_from_to(start, p, params.l_modifier || params.h_modifier ? p - 1 : 0);
+			sum += print_from_to
+			(start, p, params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
 			sum += get_print_func(p, args, &params);
 	}
